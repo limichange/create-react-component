50adffc9f3006f41781f2e44ce16dfbfe922d79b
limichange <limichange@hotmail.com>
2024-06-09 16:15:42 +0800

    feat: update tag

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ import { createReactComponent } from './create-react-component.js'
 program
   .version('1.0.0')
   .description('create react component')
-  .option('-n, --name <type>', 'Add your name')
-  .action((options) => {
-    createReactComponent(options.name)
-
-    console.log(`Done, ${options.name}!`)
-  })
+  .option('--name <type>', 'Add your name')
   .option('--names <names>', 'Add multiple names')
   .action((options) => {
-    options.names.split(',').forEach((name) => {
-      createReactComponent(name)
-    })
+    options.names &&
+      options.names.split(',').forEach((name) => {
+        createReactComponent(name)
+      })
 
-    console.log(`Done, ${options.names}!`)
+    options.name && createReactComponent(options.name)
+
+    console.log(`Done!`)
   })
 
 program.parse(process.argv)
